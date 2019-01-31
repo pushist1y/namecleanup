8c87367f160e0b8a2e0ee62edda1cfc6a5d24492
pushist1y <mister.p.sh@gmail.com>
2019-01-31 23:22:58 +0300

    init

---
+++ src/NameCleanup.Test/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NameCleanup.Test
+{
+    public static class StringExtensions
+    {
+        public static string NameCleanup(this string input)
+        {
+            var result = input;
+
+            result = result.Split(" + ").First();
+
+            result = Regex.Replace(result, @"((\d+\s*)?[xà]\s*)?\d+\s+(stuks?|paar|crackers|Zakjes|patches|Bruistabletten|Luierbroekjes|Navulmesjes|Maaltijdzakjes|Zuigtabletten|Smelttabletjes|Wasbeurten|Capsules|Rollen|Koekjes|Plakken|Luiers|Wegwerpbare\s+Zwemluiers),?", "", RegexOptions.IgnoreCase);
+            result = Regex.Replace(result, @"(\d+(,\s*\d+)?\s*cm\s*[xØ]\s*)?\d+(,\s*\d+)?\s*cm$", "");
+            result = Regex.Replace(result, @"\s+((\d+\s*)?x\s*)?(\d+[,-]\s*)?\d+\s*(kg|g|cl|ml|cm|mm|m\b|cm|l\b|liter)(?![\/])", "", RegexOptions.IgnoreCase);
+
+            result = Regex.Replace(result, @"ca\.", "");
+            result = Regex.Replace(result, @"\d+\s*x\s*$", "");
+            result = Regex.Replace(result, @"\s+x\d+$", "");
+            result = Regex.Replace(result, @"stuks?", "", RegexOptions.IgnoreCase);
+            result = Regex.Replace(result, @"Krat Bier", "", RegexOptions.IgnoreCase);
+            result = Regex.Replace(result, @"Krat", "", RegexOptions.IgnoreCase);
+            
+            result = result.Trim();
+            result = result.TrimEnd(',');
+            result = result.Trim();
+
+            return result;
+        }
+    }
+}
---
+++ src/NameCleanup.Test/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using CsvHelper;
+using Microsoft.Extensions.PlatformAbstractions;
+using Xunit;
+
+namespace NameCleanup.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestEmpty()
+        {
+            var str1 = "Robijn Wasmiddel Klein & Krachtig Stralend Wit 2 x 1, 47L";
+            var str2 = str1.NameCleanup();
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TestData()
+        {
+            var inputFilePath = FindInput();
+            Assert.NotNull(inputFilePath);
+            Assert.True(File.Exists(inputFilePath));
+            var outputFile = Path.Combine(Path.GetDirectoryName(inputFilePath), "errors.csv");
+            var sb = new StringBuilder();
+
+            using (var reader = File.OpenText(inputFilePath))
+            {
+                var records = GetRecordsFromCsv(reader);
+                var errors = new Dictionary<string, string>();
+                foreach (var record in records)
+                {
+                    string name = (string) Convert.ToString(record.name);
+                    string expected = (string) Convert.ToString(record.expected);
+
+                    Assert.NotNull(name);
+                    Assert.NotNull(expected);
+
+                    var cleaned = name.NameCleanup();
+
+                    if (cleaned != expected)
+                    {
+                        errors[name] = expected;
+                        sb.AppendLine($@"""{name}"";""{expected}"";""{cleaned}""");
+                    }
+                }
+
+                File.WriteAllText(outputFile, sb.ToString());
+                Assert.Empty(errors);
+            }
+        }
+
+        private List<dynamic> GetRecordsFromCsv(StreamReader reader)
+        {
+            var csv = new CsvReader(reader);
+            csv.Configuration.BadDataFound = null;
+            csv.Configuration.Delimiter = ",";
+            return csv.GetRecords<dynamic>().ToList();
+        }
+
+        private string FindInput()
+        {
+            var dllPath = PlatformServices.Default.Application.ApplicationBasePath;
+            var dir = new DirectoryInfo(Path.GetDirectoryName(dllPath));
+            while (true)
+            {
+                if ((dir?.EnumerateFiles("input.csv").Any()) ?? false)
+                {
+                    return Path.Combine(dir.FullName, "input.csv");
+                }
+
+                dir = dir?.Parent;
+                if (dir == null)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
